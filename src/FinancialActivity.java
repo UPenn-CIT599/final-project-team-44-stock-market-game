543c0e403ec6d8259f56a3a9ad729ec2392c205f
cingalls-git <c.w.ingalls@gmail.com>
2020-04-26 15:48:54 -0400

    javadoc for interface.

--- src/FinancialActivity.java
+++ src/FinancialActivity.java
@@ -1,4 +1,9 @@
-
+/**
+ * This interface will enable unique implementations for update cash and buySell when we
+ * add support for different asset classes in the future, which will require unique
+ * implementations.
+ *
+ */
 public interface FinancialActivity {
 	public void updateCash(double marketValue);
 	public void buySell(String name, double units);
